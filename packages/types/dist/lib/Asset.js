568631fd9dd7f2dbde0163cb3819a4042072f230
Chris <34682781+monkins1010@users.noreply.github.com>
2024-12-14 23:15:40 +0000

    Add tpes dist

---
+++ packages/types/dist/lib/Asset.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AssetTypes = exports.ChainId = void 0;
+var ChainId;
+(function (ChainId) {
+    ChainId["Bitcoin"] = "bitcoin";
+    ChainId["BitcoinCash"] = "bitcoin_cash";
+    ChainId["Ethereum"] = "ethereum";
+    ChainId["Rootstock"] = "rsk";
+    ChainId["BinanceSmartChain"] = "bsc";
+    ChainId["Near"] = "near";
+    ChainId["Polygon"] = "polygon";
+    ChainId["Arbitrum"] = "arbitrum";
+    ChainId["Solana"] = "solana";
+    ChainId["Fuse"] = "fuse";
+    ChainId["Terra"] = "terra";
+    ChainId["Avalanche"] = "avalanche";
+    ChainId["Verus"] = "verus";
+})(ChainId = exports.ChainId || (exports.ChainId = {}));
+var AssetTypes;
+(function (AssetTypes) {
+    AssetTypes["native"] = "native";
+    AssetTypes["erc20"] = "erc20";
+    AssetTypes["nft"] = "nft";
+})(AssetTypes = exports.AssetTypes || (exports.AssetTypes = {}));
+//# sourceMappingURL=Asset.js.map
